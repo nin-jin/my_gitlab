affa24464eb432f15a272701243e17f12f7ca5d6
nin-jin <nin-jin@users.noreply.github.com>
2021-02-12 07:12:07 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -4413,6 +4413,11 @@ var $;
         width_limit() {
             return Infinity;
         }
+        sub() {
+            return [
+                this.title()
+            ];
+        }
     }
     $.$mol_paragraph = $mol_paragraph;
 })($ || ($ = {}));
--- web.d.ts
+++ web.d.ts
@@ -1413,6 +1413,7 @@ declare namespace $ {
         line_height(): number;
         letter_width(): number;
         width_limit(): number;
+        sub(): readonly any[];
     }
 }
 
