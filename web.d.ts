1a2f18d758598fe3054601626ad4adbeeee855dc
nin-jin <nin-jin@users.noreply.github.com>
2021-08-22 07:13:03 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -496,7 +496,7 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_dom_render_children(el: Element, childNodes: NodeList | Array<Node | string | null>): void;
+    function $mol_dom_render_children(el: Element | DocumentFragment, childNodes: NodeList | Array<Node | string | null>): void;
 }
 
 declare namespace $ {
@@ -2551,6 +2551,10 @@ declare namespace $.$$ {
             col: string;
         }): $mol_view;
         cell_content(id: {
+            row: string[];
+            col: string;
+        }): any[];
+        cell_content_text(id: {
             row: string[];
             col: string;
         }): any[];
