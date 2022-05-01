7c3b73b46ac3136064087a66ba5a997460467080
nin-jin <nin-jin@users.noreply.github.com>
2022-05-01 16:49:18 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -6759,6 +6759,15 @@ var $;
     const { rem, px } = $mol_style_unit;
     const { hsla } = $mol_style_func;
     $mol_style_define($my_gitlab_file, {
+        box: {
+            shadow: [{
+                    x: 0,
+                    y: 0,
+                    blur: 0,
+                    spread: px(1),
+                    color: $mol_theme.line,
+                }],
+        },
         margin: $mol_gap.block,
         border: {
             radius: rem(.25),
