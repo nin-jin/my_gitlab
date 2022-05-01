747cdef2b24d9f805753a4a3091aa7709e2e31f0
jin <jin@hyoo.ru>
2022-05-01 19:47:16 +0300

    redesign

--- file/file.view.css.ts
+++ file/file.view.css.ts
@@ -5,16 +5,6 @@ namespace $ {
 	
 	$mol_style_define( $my_gitlab_file , {
 
-		box: {
-			shadow: [{
-				x: 0,
-				y: 0,
-				blur: 0,
-				spread: px(1),
-				color: $mol_theme.line,
-			}],
-		},
-
 		margin: $mol_gap.block,
 		
 		border: {
