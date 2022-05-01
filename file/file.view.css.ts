10be8cb7b433ae3f5832293ccc7cd46ea2988b4d
jin <jin@hyoo.ru>
2022-05-01 19:48:04 +0300

    Return line for demo

--- file/file.view.css.ts
+++ file/file.view.css.ts
@@ -5,6 +5,16 @@ namespace $ {
 	
 	$mol_style_define( $my_gitlab_file , {
 
+		box: {
+			shadow: [{
+				x: 0,
+				y: 0,
+				blur: 0,
+				spread: px(1),
+				color: $mol_theme.line,
+			}],
+		},
+
 		margin: $mol_gap.block,
 		
 		border: {
