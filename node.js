9415efa4d43e779a46f7eb028e7a23fa67c5c3e9
nin-jin <nin-jin@users.noreply.github.com>
2022-09-18 07:09:10 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -2652,7 +2652,7 @@ var $;
                 kids[index].force_render(path);
             }
         }
-        async ensure_visible(view, align = "start") {
+        ensure_visible(view, align = "start") {
             const path = this.view_find(v => v === view).next().value;
             this.force_render(new Set(path));
             this.dom_final();
@@ -2660,7 +2660,7 @@ var $;
         }
         bring() {
             new $mol_after_frame(() => {
-                this.dom_node().scrollIntoView();
+                this.dom_node().scrollIntoView({ inline: 'start' });
                 this.focused(true);
             });
         }
@@ -4294,7 +4294,7 @@ var $;
             return 7;
         }
         width_limit() {
-            return Infinity;
+            return +Infinity;
         }
         sub() {
             return [
@@ -7756,7 +7756,7 @@ var $;
             return "text";
         }
         length_max() {
-            return Infinity;
+            return +Infinity;
         }
         type(val) {
             if (val !== undefined)
@@ -8611,7 +8611,7 @@ var $;
                 return this.keys().map(key => this.Option(key));
             }
             option_title(key) {
-                return this.options()[key];
+                return this.options()[key] || key;
             }
         }
         __decorate([
